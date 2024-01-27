023bbb0807e70296356bd41bbf8e3a8bc90463d5
Vitek <90604277+VitekDev@users.noreply.github.com>
2024-01-27 17:23:09 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,72 @@
-const http = require('http');
+// Create web server
 
-const server = http.createServer((req, res) => {
+// Import modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const authenticate = require('../authenticate');
+const cors = require('./cors');
+
+// Import models
+const Comments = require('../models/comments');
+
+// Create router
+const commentRouter = express.Router();
+
+// Use body parser
+commentRouter.use(bodyParser.json());
+
+// Configure router
+commentRouter.route('/')
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+.get(cors.cors, (req, res, next) => {
+  // Return all comments
+  Comments.find(req.query)
+  .populate('author')
+  .then((comments) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
+    res.setHeader('Content-Type', 'application/json');
+    res.json(comments);
+  }, (err) => next(err))
+  .catch((err) => next(err));
+})
+.post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+  // Create new comment
+  if (req.body != null) {
+    req.body.author = req.user._id;
+    Comments.create(req.body)
+    .then((comment) => {
+      Comments.findById(comment._id)
+      .populate('author')
+      .then((comment) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(comment);
+      })
+    }, (err) => next(err))
+    .catch((err) => next(err));
+  } else {
+    err = new Error('Comment not found in request body');
+    err.status = 404;
+    return next(err);
+  }
+})
+.put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+  // Update all comments
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /comments');
+})
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  // Delete all comments
+  Comments.remove({})
+  .then((response) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(response);
+  }, (err) => next(err))
+  .catch((err) => next(err));
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
-});
+commentRouter.route('/:commentId')
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+.get(cors.cors())
